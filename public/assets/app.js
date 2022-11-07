ff2073d67d5b765ed42054d61ba9d6bdd909db08
Sasha Voslion <sashavoslion@gmail.com>
2022-11-07 11:41:51 +0100

    Template home version finale

---
+++ public/assets/app.js
@@ -0,0 +1,9 @@
+document.body.onload=function (){
+    nbr=3;
+    // position
+    p=0
+    // récupération des éléments
+    container=document.getElementById("container");
+    g=document.getElementById("g");
+    d=document.getElementById("d");
+}
