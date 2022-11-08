a30f603fdb8f91efeda340c327459fb560183005
Rami <ramizrkk@gmail.com>
2022-11-08 10:55:05 +0100

    Création de la partie auth

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,39 +4,43 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-
-use function PHPSTORM_META\type;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-     
-            ->add('plainPassword', PasswordType::class, [
+            ->add('email', TextType::class, [
+                'label' => "Nom d'utilisateur"
+            ])
+            ->add('plainPassword', RepeatedType::class, [
                 'mapped' => false,
-                
-                'attr' => ['autocomplete' => 'new-password'],
+                'type' => PasswordType::class,
+                'label' => 'Mot de passe',
+                'options' => [
+                    'attr' => [
+                        'type' => 'password'
+                    ]
+                ],
+                // Ajout de la case confirmer le mot de passe//
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
+                    new NotBlank(),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                        'min' => 6
+                    ])
                 ],
             ])
+            
         ;
     }
 
