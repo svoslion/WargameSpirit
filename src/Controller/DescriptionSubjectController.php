02a46a4d09176e3f332e34342cc118dabcf93bcb
Sasha Voslion <sashavoslion@gmail.com>
2022-11-09 10:06:28 +0100

    dashboard: Création du système de catégorie

--- src/Controller/DescriptionSubjectController.php
+++ src/Controller/DescriptionSubjectController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Subject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DescriptionSubjectController extends AbstractController
 {
-    #[Route('/description/subject', name: 'app_description_subject')]
-    public function index(): Response
+    #[Route('/description/subject/{slug}', name: 'app_description_subject')]
+    public function show(?Subject $subject): Response
     {
-        return $this->render('description_subject/index.html.twig', [
-            'controller_name' => 'DescriptionSubjectController',
+        if (!$subject) {
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('description_subject/show.html.twig', [
+            'subject' => $subject, 
         ]);
     }
 }
