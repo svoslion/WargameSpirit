0992a44fbf3e2f69f25f9728d6cedd33feb1358a
Sasha Voslion <sashavoslion@gmail.com>
2022-11-17 15:02:46 +0100

    Page de connexion termninée

--- src/Controller/DescriptionSubjectController.php
+++ src/Controller/DescriptionSubjectController.php
@@ -8,22 +8,28 @@ use App\Entity\Comment;
 use App\Repository\CategoryRepository;
 use App\Form\Type\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DescriptionSubjectController extends AbstractController
 {
     #[Route('/description/subject/{slug}', name: 'app_description_subject')]
-    public function show(?Subject $subject, CategoryRepository $categoryRepo): Response
+    public function show(?Subject $subject, CategoryRepository $categoryRepo, Request $request): Response
     {
         if (!$subject) {
             return $this->redirectToRoute('app_home');
         }
-
+        
+        // Partie commentaire
+        // On crée le commentaire vierge
         $comment = new Comment($subject);
 
+        // On genère le commentaire
         $commentForm = $this->createForm(CommentType::class, $comment);
- 
+
+        $commentForm->handleRequest($request);
+
         return $this->renderForm('description_subject/show.html.twig', [
             'subject' => $subject,
             'commentForm' => $commentForm,
--- src/Form/Type/CommentType.php
+++ src/Form/Type/CommentType.php
@@ -3,6 +3,7 @@
 namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
